d78e5bee56423511f3154e581de71839a4f567ee
jwhorto5 <111467209+jwhorto5@users.noreply.github.com>
2024-03-03 17:08:19 -0500

    Moved camera code to new script

--- Mobile Defense/Assets/Scripts/Director.cs
+++ Mobile Defense/Assets/Scripts/Director.cs
@@ -43,10 +43,6 @@ public class Director : MonoBehaviour
     /// </summary>
     private int wave = 0;
 
-    public float zoomOutMin = 1;
-    public float zoomOutMax = 8;
-    Vector3 touchStart;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -69,37 +65,6 @@ public class Director : MonoBehaviour
         }
 
         countdown -= Time.deltaTime;
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            touchStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        }
-        if (Input.touchCount == 2)
-        {
-            Touch touchZero = Input.GetTouch(0);
-            Touch touchOne = Input.GetTouch(1);
-
-            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
-            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
-
-            float prevMagnitude = (touchZeroPrevPos - touchOnePrevPos).magnitude;
-            float currentMagnitude = (touchZero.position - touchOne.position).magnitude;
-
-            float difference = currentMagnitude - prevMagnitude;
-
-            zoom(difference * 0.01f);
-        }
-        else if (Input.GetMouseButton(0))
-        {
-            Vector3 direction = touchStart - Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Camera.main.transform.position += direction;
-        }
-        zoom(Input.GetAxis("Mouse ScrollWheel"));
-    }
-
-    void zoom(float increment)
-    {
-        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - increment, zoomOutMin, zoomOutMax);
     }
 
     IEnumerator SpawnWave()
