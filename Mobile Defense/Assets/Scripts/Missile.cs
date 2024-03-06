cbbe87c3efe4ba6523055d965aa19556067a756c
jwhorto5 <111467209+jwhorto5@users.noreply.github.com>
2024-03-06 14:03:40 -0500

    Implemented missile splash damage

--- Mobile Defense/Assets/Scripts/Missile.cs
+++ Mobile Defense/Assets/Scripts/Missile.cs
@@ -1,8 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Missile : Bullet
 {
-    
+    public float explosionRadius = 5f;
+
+    public override void HitTarget()
+    {
+        //check if we hit an enemy
+        Enemy eScript;
+        if (target.TryGetComponent<Enemy>(out eScript))
+        {
+            //if so, check if any enemy colliders are within the
+            //explosion radius
+            Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+            foreach(Collider c in colliders)
+            {
+                if (c.gameObject.TryGetComponent<Enemy>(out eScript))
+                {
+                    //make all these enemies take damage
+                    eScript.TakeDamage(damage);
+                }
+            }
+            Debug.Log("Hit");
+        }
+    }
 }
